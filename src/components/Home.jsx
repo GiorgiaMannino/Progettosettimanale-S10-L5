6776729f421d5d922cc23cd81344112a923fd2ef
GiorgiaMannino <giorgimannino@hotmail.com>
2025-02-14 12:46:45 +0100

    fetch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,37 +1,6 @@
-import { useState } from "react";
-import { Container, Form } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import Welcome from "./Welcome";
-
-const SearchBar = ({ changeCity }) => {
-  const [city, setCity] = useState("");
-
-  const handleChange = (e) => {
-    const cityName = e.target.value;
-    setCity(cityName);
-    changeCity(cityName);
-  };
-
-  return (
-    <div style={{ position: "relative" }}>
-      <Form.Control
-        className="mt-4 ps-5"
-        type="text"
-        placeholder="Search for a city..."
-        value={city}
-        onChange={handleChange}
-      />
-      <i
-        className="bi bi-search"
-        style={{
-          position: "absolute",
-          left: "10px",
-          top: "50%",
-          transform: "translateY(-50%)",
-        }}
-      ></i>
-    </div>
-  );
-};
+import SearchBar from "./SearchBar";
 
 const Home = () => {
   const searchCity = (city) => {
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const SearchBar = ({ changeCity }) => {
+  const [city, setCity] = useState("");
+
+  const fetchWeather = async () => {
+    if (!city) return;
+    try {
+      const response = await fetch(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=bb5d1147afc96159e576456b97bf104c`
+      );
+      const [location] = await response.json();
+
+      if (location) {
+        const { lat, lon } = location;
+
+        const weatherCity = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=bb5d1147afc96159e576456b97bf104c&units=metric`
+        );
+        const currentWeather = await weatherCity.json();
+
+        changeCity({
+          city,
+          lat,
+          lon,
+          weather: currentWeather,
+        });
+      } else {
+        console.error("città non presente");
+      }
+    } catch (error) {
+      console.error("errore", error);
+    }
+  };
+  return (
+    <div style={{ position: "relative" }}>
+      <Form.Control
+        className="mt-4 ps-5"
+        type="text"
+        placeholder="Search for a city..."
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+        onKeyDown={(e) => e.key === "Enter" && fetchWeather()}
+      />
+      <i
+        className="bi bi-search"
+        onClick={fetchWeather}
+        style={{
+          position: "absolute",
+          left: "10px",
+          top: "50%",
+          transform: "translateY(-50%)",
+        }}
+      ></i>
+    </div>
+  );
+};
+
+export default SearchBar;
