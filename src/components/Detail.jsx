c2a6486d89f8818fa034c7cc13247b4fd2c654c1
GiorgiaMannino <giorgimannino@hotmail.com>
2025-02-14 13:27:14 +0100

    organizzazione pagina detail

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,9 +1,30 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col, Card, ListGroup } from "react-bootstrap";
 
 const Detail = () => {
   return (
-    <Container>
-      <p>Pagina Dettaglio</p>
+    <Container className="mt-4">
+      <Row>
+        <Col sm={12} md={6}>
+          <Card>
+            <Card.Body>
+              <Card.Title className="d-flex align-items-center">CTTAà</Card.Title>
+              <Card.Text>CONDIZIONI METEO</Card.Text>
+              <Card.Text>GRADI</Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+
+        <Col sm={12} md={6}>
+          <Card className="mb-4 ">
+            <Card.Body>
+              <Card.Title>PREVISIONI DEI 5 GIORNIO SUCCESSIVI</Card.Title>
+              <ListGroup variant="flush">
+                <ListGroup.Item> GIORNI</ListGroup.Item>
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 };
